1a9aa1cad90c5ab0898a625a4b3238810c6d29b6
Edgar Olvera <eols7767@gmail.com>
2021-08-02 21:59:48 -0500

    Add files via upload

---
+++ TaskScheduler_RazorProject/TaskScheduler_RazorProject/Model/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskScheduler_RazorProject.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Task> Task { get; set; }
+
+    }
+}
---
+++ TaskScheduler_RazorProject/TaskScheduler_RazorProject/Model/Task.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskScheduler_RazorProject.Model
+{
+    public class Task
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public DateTimeOffset DueDate { get; set; }
+
+        [Required]
+        public DateTimeOffset DueTime { get; set; }
+
+        [Required]
+        public string Color { get; set; }
+    }
+}
