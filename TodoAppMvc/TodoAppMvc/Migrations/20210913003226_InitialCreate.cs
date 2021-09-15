a6cb4748e0bbcdb30cd814aab580f4e2b6abc832
Edgar Olvera <eols7767@gmail.com>
2021-09-14 20:11:52 -0500

    Add files via upload

---
+++ TodoAppMvc/TodoAppMvc/Migrations/20210913003226_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TodoAppMvc.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Event",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    DueDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Color = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Event", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Event");
+        }
+    }
+}
---
+++ TodoAppMvc/TodoAppMvc/Models/Event.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoAppMvc.Models
+{
+    public class Event
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        [Display(Name ="Due Date")]
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime DueDate { get; set; }
+
+        [Display(Name = "Due Time")]
+        [DataType(DataType.Time)]
+        [Required]
+        public DateTime DateTime { get; set; }
+
+        public string Color { get; set; }
+    }
+}
