32b51459ee452c0ceb7acada1c1b0b9c18cb1985
Edgar Olvera <eols7767@gmail.com>
2021-09-14 22:42:30 -0500

    Update HomeController.cs

--- TodoAppMvc/TodoAppMvc/Controllers/HomeController.cs
+++ TodoAppMvc/TodoAppMvc/Controllers/HomeController.cs
@@ -4,14 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+// Our home page solely lists the number of events our database. 
+//  So our controller returns the count of our database using the db context.
 
 namespace TodoAppMvc.Controllers
 {
     public class HomeController : Controller
     {
         private readonly MvcEventsContext _context;
-
+        
+        //  configure dependency injection to access db context 
         public HomeController(MvcEventsContext context)
         {
             _context = context;
@@ -19,7 +21,9 @@ namespace TodoAppMvc.Controllers
 
         // GET: /<controller>/
         public IActionResult Index()
-        {
+        {   
+            //  use the count() method to get number of events
+            //  return events to the view home page
             return View( _context.Event.Count());
         }
     }
